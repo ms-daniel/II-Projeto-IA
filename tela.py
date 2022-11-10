0ddf7b9dd77f86ed230c198f4ea988c51d099745
Carlos Daniel <ms_danniel@outlook.com>
2022-11-10 16:42:43 -0300

    teste com pack e place

--- tela.py
+++ tela.py
@@ -1,14 +1,20 @@
 import tkinter as tk
+from tkinter import ttk
 
 class App(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
-        
+        button = tk.Button(
+                            master, text="Quit", width=20, command=master.destroy,
+                            height=1, bg="red3", fg="white"
+                          )
+        button.pack(side="bottom", pady=2)
 
-
+        button2 = tk.Button(master, text='dan')
+        button2.place(x=0,y=0)
 
 root = tk.Tk()
-root.title('Diabético?')
+root.title('Testing Classifiers')
 root.geometry('600x400')
 root.resizable(False, False)
 icon = tk.PhotoImage(file = "icon.png")
