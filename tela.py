f002edfc46ac767a35abb1cff62183cc4109037c
Carlos Daniel de Jesus <ms_danniel@outlook.com>
2022-11-11 23:33:21 -0300

    adicionado cursores aos campos clicaveis e não clicaveis

--- tela.py
+++ tela.py
@@ -16,7 +16,7 @@ class App(tk.Frame):
       self.linkArea.bind('<Return>', lambda x: self.validateLink(self.link))
       self.linkArea.place(x = 3, y = 20)
       
-      loadButton = tk.Button(master, text="Load", command = lambda: self.validateLink(self.link), bg = "ForestGreen")
+      loadButton = tk.Button(master, text="Load", command = lambda: self.validateLink(self.link), bg = "ForestGreen", cursor="hand2")
       loadButton.place(x = 410, y = 18)
 
       ############### radiobuttons para o separador ###############
@@ -40,7 +40,7 @@ class App(tk.Frame):
       percepLabel.place(x = 47, y = 73)
  
       self.percepText = tk.StringVar()
-      percepBox = tk.Entry(master, textvariable=self.percepText, width=13, border = 2, relief = "ridge", highlightthickness=1, highlightbackground="blue", state="disabled")
+      percepBox = tk.Entry(master, textvariable=self.percepText, width=13, border = 2, relief = "ridge", highlightthickness=1, highlightbackground="blue", state="disabled", cursor="arrow")
       percepBox.place(x = 50, y = 93)
 
       #--------------#
@@ -49,7 +49,7 @@ class App(tk.Frame):
       knnLAbel.place(x = 160, y = 73)
 
       self.knnText = tk.StringVar()
-      knnBox = tk.Entry(master, textvariable=self.knnText, width=13, border = 2, relief = "ridge", highlightthickness=1, highlightbackground="blue", state="disabled")
+      knnBox = tk.Entry(master, textvariable=self.knnText, width=13, border = 2, relief = "ridge", highlightthickness=1, highlightbackground="blue", state="disabled", cursor="arrow")
       knnBox.place(x = 178, y = 93)
 
       #--------------#
@@ -58,18 +58,18 @@ class App(tk.Frame):
       oneRLabel.place(x = 310, y = 73)
 
       self.oneRText = tk.StringVar()
-      oneRBox = tk.Entry(master, textvariable=self.oneRText, width=13, border = 2, relief = "ridge", highlightthickness=1, highlightbackground="blue", state="disabled")
+      oneRBox = tk.Entry(master, textvariable=self.oneRText, width=13, border = 2, relief = "ridge", highlightthickness=1, highlightbackground="blue", state="disabled", cursor="arrow")
       oneRBox.place(x = 311, y = 93)
 
       ###############################################################
-      self.doitButton = tk.Button(master, text="Do It!", command=None, font = ("Comic Sans",14), bg="green", fg="white", state="disabled")
+      self.doitButton = tk.Button(master, text="Do It!", command=None, font = ("Comic Sans",14), bg="green", fg="white", state="disabled", cursor="arrow")
       self.doitButton.place(x = 190, y = 123)
       
 
       #botão quit
       quitButton = tk.Button(
                           master, text="Quit", width=20, command=master.destroy,
-                          height=1, bg="red3", fg="white"
+                          height=1, bg="red3", fg="white", cursor="hand2"
                         )
       quitButton.pack(side="bottom", pady=2)
 
@@ -83,14 +83,15 @@ class App(tk.Frame):
       try:
         arq = pd.read_csv(url, sep = None, encoding='latin1')
       except:
-        self.doitButton.config(state="disabled")
+        self.doitButton.config(state="disabled", cursor="arrow")
+        self.RadioPontoVirgula.config(state="disabled")
+        self.RadioVirgula.config(state="disabled")
         messagebox.showerror(title="Warning!", message= "Possible problems:\n" +
                                                         "1 - File does not exist in link/directory\n"+
                                                         "2 - File is not .csv\n"+
                                                         "3 - Unable to access the link")
       else:
-        self.doitButton.config(state="normal")
-
+        self.doitButton.config(state="normal", cursor="hand2")
         self.RadioPontoVirgula.config(state="normal")
         self.RadioVirgula.config(state="normal")
 
