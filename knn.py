b397e49eb78c27ab73bf3b839bff2345385beee2
Carlos Daniel de Jesus <ms_danniel@outlook.com>
2022-11-14 11:31:30 -0300

    adicionado arg neighbors para quantidade de "vizinhos"

--- knn.py
+++ knn.py
@@ -1,7 +1,7 @@
 from sklearn.neighbors import KNeighborsClassifier
 
 
-def learn(atributos_norm, diagnostico_norm):
-    modelo = KNeighborsClassifier(n_neighbors=1)
+def learn(atributos_norm, diagnostico_norm, neighbors):
+    modelo = KNeighborsClassifier(n_neighbors=neighbors)
     modelo.fit(atributos_norm, diagnostico_norm)
     return ('%.2f' % (modelo.score(atributos_norm, diagnostico_norm) * 100) + '%')
