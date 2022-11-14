2248c013414e3fa8cf39dc359681f85f29a8fb20
Carlos Daniel de Jesus <ms_danniel@outlook.com>
2022-11-14 11:37:17 -0300

    usando lista de stringVar ao inves de 3 args

--- back_test.py
+++ back_test.py
@@ -1,19 +1,18 @@
 import runit
-
+from tkinter import StringVar
 #
 #
-def test(url, separator, neighbors, varPercept, varKnn, varOneR):
+def test(url, separator, neighbors, listVar):
     """
         calculates the accuracy for the classifierless perceptron model, with the KNN classifier and the One Rule
 
         :param url:         string
         :param separator:   string
         :param neighbors:   int
-        :param varPercept:  StringVar(tkinter)
-        :param varKnn:      StringVar(tkinter)
-        :param varOneR:     StringVar(tkinter)
+        :param listVar:     StringVar list(0 = perceptron, 1 = knn, 2 = oneRule)
     """
-    runit.run_it(url)
-    print(runit.accuratePerceptron)
-    print(runit.accurateKNN)
-    print(runit.accurateOneR)
+    runit.run_it(url, separator, neighbors)
+
+    listVar[0].set(runit.accuratePerceptron)
+    listVar[1].set(runit.accurateKNN)
+    listVar[2].set(runit.accurateOneR)
